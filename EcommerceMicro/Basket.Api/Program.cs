f38ebe9350aa40585d7c5c87119bd963e4769e1b
phieravol <121568897+phieravol@users.noreply.github.com>
2023-08-28 22:18:43 +0700

    t6 - Add Basket repository

--- EcommerceMicro/Basket.Api/Program.cs
+++ EcommerceMicro/Basket.Api/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.Api", Version = "v1" });
+});
+
+// Add service StackExchangeRedisCache
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration = builder.Configuration.GetConnectionString("RedisCacheConnection");
+});
 
 var app = builder.Build();
 
@@ -13,7 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v11/swagger.json", "Basket.Api v1"));
 }
 
 app.UseAuthorization();
---
+++ EcommerceMicro/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,62 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.Api.Repositories
+{
+	public class BasketRepository : IBasketRepository
+	{
+		private readonly IDistributedCache redisCache;
+
+		public BasketRepository(IDistributedCache redisCache)
+		{
+			this.redisCache = redisCache;
+		}
+
+		/// <summary>
+		/// Delete basket by Username
+		/// </summary>
+		/// <param name="userName"></param>
+		/// <returns></returns>
+		public async Task DeleteBasket(string userName)
+		{
+			await redisCache.RemoveAsync(userName);
+		}
+
+		/// <summary>
+		/// Get Basket by Username
+		/// </summary>
+		/// <param name="userName"></param>
+		/// <returns></returns>
+		public async Task<ShoppingCart> GetBasket(string userName)
+		{
+			//get basket as string type by username
+			var rawBasket = await redisCache.GetStringAsync(userName);
+
+			//return empty basket when string is empty
+			if (String.IsNullOrEmpty(rawBasket))
+			{
+				return null;
+			}
+
+			//convert basket from json into object
+			var basket = JsonConvert.DeserializeObject<ShoppingCart>(rawBasket);
+			return basket;
+		}
+
+		/// <summary>
+		/// Update Basket when shopping-item changeds 
+		/// </summary>
+		/// <param name="basket"></param>
+		/// <returns></returns>
+		public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+		{
+			//Convert basket into json format
+			var jsonBasket = JsonConvert.SerializeObject(basket);
+			//Update basket by key and value
+			redisCache.SetStringAsync(basket.UserName, jsonBasket);
+			//Return current basket
+			return GetBasket(basket.UserName);
+		}
+	}
+}
