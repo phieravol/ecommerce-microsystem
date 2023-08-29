c5ffdabdbcf57cb98ae5465fcabbd64f5997d5cc
phieravol <121568897+phieravol@users.noreply.github.com>
2023-08-29 12:27:05 +0700

    t7 - create basket api

---
+++ EcommerceMicro/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+	[Route("api/v1/[controller]")]
+	[ApiController]
+	public class BasketController : ControllerBase
+	{
+		private readonly IBasketRepository repository;
+
+		public BasketController(IBasketRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		[HttpGet("{username}")]
+		public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+		{
+			var basket = await repository.GetBasket(username);
+			return Ok(basket ?? new ShoppingCart(username));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+		{
+			var currentBasket = await repository.UpdateBasket(basket);
+			return Ok(currentBasket);
+		}
+
+		[HttpDelete("{username}")]
+		public async Task<IActionResult> DeleteBasket(string username)
+		{
+			await repository.DeleteBasket(username);
+			return NoContent();
+		}
+	}
+}
--- EcommerceMicro/Basket.Api/Program.cs
+++ EcommerceMicro/Basket.Api/Program.cs
@@ -1,15 +1,16 @@
+using Basket.Api.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
 	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.Api", Version = "v1" });
 });
+builder.Services.AddTransient<IBasketRepository, BasketRepository>();
 
 // Add service StackExchangeRedisCache
 builder.Services.AddStackExchangeRedisCache(options =>
@@ -23,7 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v11/swagger.json", "Basket.Api v1"));
+	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.Api v1"));
 }
 
 app.UseAuthorization();
