2c984c98d3b6a3127ba5e1ff4c5c738bfc1abdef
phieravol <phinqevol@gmail.com>
2024-01-01 21:33:34 +0700

    t14: basket db - connect discount client

--- EcommerceMicro/Basket.Api/Controllers/BasketController.cs
+++ EcommerceMicro/Basket.Api/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.Api.Entities;
+using Basket.Api.GrpcServices;
 using Basket.Api.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace Basket.Api.Controllers
 	public class BasketController : ControllerBase
 	{
 		private readonly IBasketRepository repository;
+		private readonly DiscountGrpcService discountGrpcService;
 
-		public BasketController(IBasketRepository repository)
+		public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
 		{
 			this.repository = repository;
+			this.discountGrpcService = discountGrpcService;
 		}
 
 		[HttpGet("{username}")]
@@ -26,6 +29,14 @@ namespace Basket.Api.Controllers
 		[HttpPost]
 		public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
 		{
+			// TODO: Communicate with Discount.Grpc
+			// and Calculate lastest prices of product into shopping cart
+			// consume discount Grpc
+			foreach (var item in basket.Items)
+			{
+				var coupon = await discountGrpcService.GetDiscount(item.ProductName);
+				item.Price -= coupon.Amount;
+			}
 			var currentBasket = await repository.UpdateBasket(basket);
 			return Ok(currentBasket);
 		}
